fcdf4953f8b3261b703d10f1bcda8078c68bffae
mdlawrenceusa <mdlawrence@me.com>
2020-11-05 12:24:01 -0500

    end-of-class

---
+++ src/app/graphic-design/graphic-design.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-graphic-design',
+  templateUrl: './graphic-design.component.html',
+  styleUrls: ['./graphic-design.component.css']
+})
+export class GraphicDesignComponent implements OnInit {
+  shippingCosts;
+
+  constructor(
+    private cartService: CartService
+  ) { }
+
+  ngOnInit() {
+    this.shippingCosts = this.cartService.getP();
+  }
+
+}
